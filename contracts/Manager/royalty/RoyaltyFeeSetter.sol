7eaede46a867637cac534d39c4ac939f2841999f
Remix Workspace <>
2022-09-17 02:03:20 +0800

    add

---
+++ contracts/Manager/royalty/RoyaltyFeeSetter.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import {IERC165} from "@openzeppelin/contracts/utils/introspection/IERC165.sol";
+
+import {IRoyaltyFeeRegistry} from "./interface/IRoyaltyFeeRegistry.sol";
+import {IOwnable} from "../interface/IOwnable.sol";
+
+//  Register royalty fee
+contract RoyaltyFeeSetter is Ownable {
+  
+}
